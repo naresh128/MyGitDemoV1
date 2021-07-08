31cc3a21098feb34ca4967754f04868ef056a32d
naresh128 <85680658+naresh128@users.noreply.github.com>
2021-07-08 18:18:00 +0530

    Update Test.java
    Made changes in Master remotely itself..

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("main Class../I am Naresh");
 		System.out.println("Added another logger..");
+		System.out.println("Making change in Github Remote itself..");
 	}
 }
