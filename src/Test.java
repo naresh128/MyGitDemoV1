4ac4d740bb66681a4740ed9091c2a8908c7e0e4c
naresh.kumar <naresh.kumar@WKSBAN292428931.corp.mphasis.com>
2021-07-08 18:06:14 +0530

    Added another logging statement..

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("main Class..");
+		System.out.println("main Class../I am Naresh");
+		System.out.println("Added another logger..");
 	}
 }
