425914fb3195524d80b5afaff7fd5cf380366696
naresh128 <85680658+naresh128@users.noreply.github.com>
2021-07-08 19:18:34 +0530

    Update Test.java
    Calling disp method in Main Class.

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,8 @@ public class Test {
 		Test1 t=new Test1();
 		t.show();
 		System.out.println("Calling show metho from Test1..");
+		Test1 t1=new Test1();
+		t1.disp();
+		System.out.println("Calling disp metho from Test1..");
 	}
 }
