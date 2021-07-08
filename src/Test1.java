4135e8606a13d6a83c08065c666a57e37346486b
naresh.kumar <naresh.kumar@WKSBAN292428931.corp.mphasis.com>
2021-07-08 19:17:06 +0530

    remove log

--- src/Test1.java
+++ src/Test1.java
@@ -1,11 +1,8 @@
 
 public class Test1 {
-public void show()
-{
+	public void show() {
 
-	System.out.println("show method getting called..");
+		System.out.println("In show() method");
 
-	System.out.println("In show() method");
-
-}
+	}
 }
